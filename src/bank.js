a41a51ce02ca7e6d465477a19e27f998656497aa
Siyabonga Lukhele <siyabonga.lukhele@umuzi.org>
2025-07-27 20:23:22 +0200

    feat: openBankAccount and addAccountType logic and tests

--- src/bank.js
+++ src/bank.js
@@ -1,12 +1,38 @@
-import { BankAccount } from "./bank_account";
+import { BankAccount } from "./bank_account.js";
 
-const getInterest = ({ accountType }, accountTypes) => {
+export const BANK_ERROR_MESSAGES = {
+  ACCOUNT_TYPE_EXISTS: "Account type already exists",
+  NEGATIVE_INTEREST_RATE: "Interest rate cannot be negative",
+  INVALID_ACCOUNT_TYPE: "Invalid account type",
+};
+
+const getInterest = (accountType, accountTypes) => {
   for (const type of accountTypes) {
-    if (type === accountType) {
-      return BankAccount.getInterestRate(type);
+    if (type.accountType === accountType) {
+      return type.interestRate;
     }
   }
-  throw new Error("Invalid account type");
+  throw new Error(BANK_ERROR_MESSAGES.INVALID_ACCOUNT_TYPE);
+};
+
+const isAccNumberTaken = (accNumber, accounts) => {
+  for (const account of accounts.values()) {
+    if (account.accountNumber === accNumber) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const generateAccNumber = () =>
+  Array.from({ length: 10 }, () => Math.floor(Math.random() * 10)).join("");
+
+const generateUniqueAccNumber = (accounts) => {
+  let accNumber;
+  do {
+    accNumber = generateAccNumber();
+  } while (isAccNumberTaken(accNumber, accounts));
+  return accNumber;
 };
 
 export class Bank {
@@ -22,12 +48,36 @@ export class Bank {
   }
 
   addAccountType({ accountType, interestRate }) {
-    if (this.#accountTypes.has(accountType)) {
-      throw new Error("Account type already exists");
+    for (const type of this.#accountTypes) {
+      if (type.accountType === accountType) {
+        throw new Error(BANK_ERROR_MESSAGES.ACCOUNT_TYPE_EXISTS);
+      }
     }
     if (interestRate < 0) {
-      throw new Error("Interest rate cannot be negative");
+      throw new Error(BANK_ERROR_MESSAGES.NEGATIVE_INTEREST_RATE);
     }
-    this.#accountTypes.add(accountType);
+    this.#accountTypes.add({ accountType, interestRate });
+  }
+
+  openBankAccount({ accountType }) {
+    let accountTypeExists = false;
+    for (const type of this.#accountTypes) {
+      if (type.accountType === accountType) {
+        accountTypeExists = true;
+        break;
+      }
+    }
+    if (!accountTypeExists) {
+      throw new Error(BANK_ERROR_MESSAGES.INVALID_ACCOUNT_TYPE);
+    }
+    const accountNumber = generateUniqueAccNumber(this.#accounts);
+    const interestRate = getInterest(accountType, this.#accountTypes);
+    const account = new BankAccount({
+      accountType,
+      accountNumber,
+      interestRate,
+    });
+    this.#accounts.set(accountNumber, account);
+    return accountNumber;
   }
 }
